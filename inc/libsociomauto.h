447781fe28affb4e9fb809198af399253c1ea6c1
Don Duvall <dduvall@psft.local>
2018-06-06 07:50:09 -0700

    initial project commit

---
+++ inc/libsociomauto.h
@@ -0,0 +1,15 @@
+#ifndef __LIBSOCIOMAUTO_H_
+#define __LIBSOCIOMAUTO_H_
+
+/*
+    Single header file for using shared object which simplifies
+    the communication to modules and the system over the backplane.
+
+    Essentially libsociomauto is a client library to aid in the
+    development of modules.
+*/
+
+
+
+
+#endif
